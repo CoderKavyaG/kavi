ae2e9c8792f71cb85301e6cdc59f4889855c2803
CoderKavyaG <coderkavyag@gmail.com>
2025-09-12 04:49:30 +0530

    fixed github page error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser'
+  },
   server: {
     port: 3000,
     open: true
